7f96ad69d2c7bdcfde7c6cbf61f2e706abdc5566
peternjagi <peternjagi17@gmail.com>
2018-05-04 14:26:50 +0300

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function() {
+    $("#button").click(function() {
+      var ping= parseInt($("#mynumbers").val());
+      pong(ping);
+      yourNumbers.forEach(function(ping){
+        $("#result").append("<li>" + ping + "</li>");
+      });
+    });
+  });
