f4877dc8ea8a4c986ecfebb4fbbc4f89cc9de87b
peternjagi <peternjagi17@gmail.com>
2018-05-04 14:40:49 +0300

    add user and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
-$(document).ready(function() {
+//user interface logic
+
+  $(document).ready(function() {
     $("#button").click(function() {
-      var ping= parseInt($("#mynumbers").val());
-      pong(ping);
-      yourNumbers.forEach(function(ping){
-        $("#result").append("<li>" + ping + "</li>");
+      var pong= parseInt($("#mynumbers").val());
+      ping(pong);
+      yourNumbers.forEach(function(pong){
+        $("#result").append("<li>" + pong + "</li>");
       });
     });
   });
+
+  //business logic
+var yourNumbers=[];
+function ping(pong) {
+  for (var index=1; index<=pong; index++)
+if (index%15===0) {
+	yourNumbers.push("ping-pong");
+}
+  else if (index %5===0){
+   yourNumbers.push("pong");
+}
+  else if (index %3===0){
+	yourNumbers.push("ping");
+}else {
+  yourNumbers.push(index);
+}
+
+}
